984937ff671482430908794c8a3e6c7a1c56d7ba
ferhatbostanci <ferhatbostanciofficial@gmail.com>
2019-06-15 10:37:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# O-Elini-Indir-JS
+
+Biraz yapa yeka biraz kod = eğlence
+
+[Demo](https://o-elini-indir-js.herokuapp.com)
---
+++ js/app.js
@@ -0,0 +1,29 @@
+const video = document.querySelector('#video');
+const audio = document.querySelector('#audio');
+const canvas = document.querySelector('#canvas');
+const context = canvas.getContext('2d');
+const modelParams = {flipHorizontal: true, imageScaleFactor: 0.7, maxNumBoxes: 20, iouThreshold: 0.5, scoreThreshold: 0.79,};
+let model;
+
+handTrack.startVideo(video).then(status => {
+    if(status){
+        navigator.getUserMedia({video: {}}, stream => {
+                video.srcObject = stream;
+                setInterval(runDetection, 100);
+            }, error => console.log(error)
+        );
+    }
+});
+
+function runDetection() {
+    model.detect(video).then(predictions => {
+        //model.renderPredictions(predictions, canvas, context, video);
+        if(predictions.length > 0 && audio.paused){
+            audio.play();
+        }
+    });
+}
+
+handTrack.load(modelParams).then(lmodel => {
+    model = lmodel;
+});
